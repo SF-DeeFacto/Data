f42bbc8bfd3cfc6b4060d75120ddda64e9f21798
Tndals61 <sungmin.jeon.rafael@gmail.com>
2025-07-22 11:29:05 +0900

    Sensor Data Generator - Temperature
    온도 센서 데이터 (CSV)와 Temp 데이터 제너레이터 (java) 버전 1.
    현재 1시간의 데이터를 만들게 코딩 되어 있음

---
+++ data_gen/sensorDataGen/src/main/java/com/deefacto/sensorDataGen/SensorDataGenApplication.java
@@ -0,0 +1,13 @@
+package com.deefacto.sensorDataGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDataGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDataGenApplication.class, args);
+	}
+
+}
---
+++ data_gen/sensorDataGen/src/test/java/com/deefacto/sensorDataGen/SensorDataGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deefacto.sensorDataGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDataGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
